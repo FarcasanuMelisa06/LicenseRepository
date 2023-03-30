420c6074bb9c66c8be05a936c00032a1f0b3876a
Melisa Farcasanu <melisa_farcasanu@yahoo.com>
2023-03-30 19:30:39 +0300

    add new endpoint

--- Server/src/main/java/com/example/test3json/controller/ChapterController.java
+++ Server/src/main/java/com/example/test3json/controller/ChapterController.java
@@ -3,9 +3,11 @@ package com.example.test3json.controller;
 import com.example.test3json.model.Chapter;
 import com.example.test3json.repository.ChaptersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/chapters")
@@ -16,6 +18,15 @@ public class ChapterController {
     @GetMapping()
     public List<Chapter> getCapitole() {
         return chaptersRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Chapter> getChapterById(@PathVariable Long id) {
+        Optional<Chapter> chapterOptional = chaptersRepository.findById(id);
+        if (chapterOptional.isPresent()) {
+            return ResponseEntity.ok(chapterOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 
