d662fbf7228ee1e0948a1367a03316f2e6f6c13d
Melisa Farcasanu <melisa_farcasanu@yahoo.com>
2023-06-17 11:11:14 +0300

    AppIcon&KeyboardSettings

---
+++ App/Network/Extensions/UIViewControllerExtension.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewControllerExtension.swift
+//  TestProject
+//
+//  Created by Melisa Farcasanu on 17.06.2023.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func setupHideKeyboardOnTap() {
+        self.view.addGestureRecognizer(self.endEditingRecognizer())
+        self.navigationController?.navigationBar.addGestureRecognizer(self.endEditingRecognizer())
+    }
+    
+    private func endEditingRecognizer() -> UIGestureRecognizer {
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(self.view.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        return tap
+    }
+}
